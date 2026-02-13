7416778ab9be2c552bcd76b8dc72f413c40d8dbc
Eugene Kimathi <eugenekimathi@gmail.com>
2026-02-13 18:20:59 +0300

    Add displayRecipe function

--- script.js
+++ script.js
@@ -36,4 +36,78 @@ let recipes = JSON.parse(localStorage.getItem("recipes")) || [];
 
 // Add new recipe
 recipes.push(recipe);
+
+//save back to localstorage
+localStorage.setItem("recipes" , JSON.stringify(recipes));
+
+//show success message
+showToast("✅ Recipe saved successfully!" , "success");
+
+// Clear the form 
+document.getElementById("recipeForm").reset ();
+
+// Redirect to recipes page after 1.5 sec
+setTimeout(() =>{
+   window.location.href = "recipes.html" ;
+}, 1500); 
 }
+
+// Display all  recipes on the recipes.html page
+
+function displayRecipes() {
+    // Get recipes from localStorage
+    const recipes = JSON.parse(localStorage.getItem("recipes")) || [];
+
+    //Get DOM elements
+    const list = document.getElementById("recipeList");
+    const emptyState = document.getElementById("emptyState");
+
+    // Clear current content
+    list.innerHTML ="";
+
+    // Show/hide empty state
+    if ( recipes.length ===0) {
+        emptyState.classList.remove("hidden");
+        list.style.display = "none";
+    } else {
+        emptyState.classList.add("hidden");
+        list.style.display = "grid";
+
+        // Create a card for each recipe
+        recipes.forEach((recipe, index) => {
+          const card = document.createElement("div");
+          
+          // Build the card HTML
+          card.innerHTML = `
+          ${recipe.imageUrl
+             ?`<img scr="${recipe.imageUrl}" alt="${recipe.name}" class="recipe-image">`
+             : `<div class="recipe-image">🍳</div>`
+          }
+          <div class="recipe-body">
+            <h3 class="recipe-title">${recipe.name}</h3>
+          
+            <div class="recipe-meta">
+                <span class="meta-badge">⏱️${recipe.time}mins</span>
+                ${recipe.servings ? `<span class="meta-badge">🍽️ ${recipe.servings} servings</span>` : ''}
+            </div>
+
+            <div class="recipe-ingredients">
+            <strong>Ingredients:</strong><br>
+            ${recipe.ingredients.replace(/\n/g, '<br>')}
+            </div>
+
+            ${recipe.notes ? `<div class="recipe.notes">${recipe.notes}</div>` : ''}
+
+            <div class="recipe-footer">
+                <button class="btn-delete" onclick="deleteRecipe(${index})"> 🚮 Delete Recipe </button>
+            </div>
+          </div>
+
+
+          `;
+
+          list.appendChild(card);
+        });
+            
+        }
+    }
